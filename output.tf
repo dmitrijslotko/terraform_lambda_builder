fee9d181269294cb0fbe71ccf71d9b6c7bad9474
Dmitrijs Lotko <dmitrijs.lotko@google.com>
2022-02-17 12:55:43 +0200

    output update

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "lambda_output" {
-  value = aws_lambda_function.lambda[0]
+  value = var.deploy_mode == "SAM" ? data.aws_lambda_function.existing : aws_lambda_function.lambda[0]
+}
+
+data "aws_lambda_function" "existing" {
+  function_name = var.function_name
 }
