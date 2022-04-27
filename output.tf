409d73e8368480e168d0a9eea761710031d1e8b8
dmitrijslotko <dmitrijs.lotko@1nce.com>
2022-04-27 15:34:15 +0300

    added alias output

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "lambda" {
   value = aws_lambda_function.lambda
 }
 
+output "alias" {
+  value = aws_lambda_alias.lambda_alias
+}
+
 output "role" {
   value = aws_iam_role.lambda_builder_iam_role
 }
