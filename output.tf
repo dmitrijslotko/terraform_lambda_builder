4ca3a0cd6759b1d56d8c4f8cde7b47ed7fef2b44
dmitrijslotko <dmitrijs.lotko@1nce.com>
2022-04-29 15:46:15 +0300

    output updated

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "alias" {
 output "role" {
   value = aws_iam_role.lambda_builder_iam_role
 }
+
+output "arn" {
+  value = var.alias != null ? aws_lambda_alias.lambda_alias[0].arn : aws_lambda_function.lambda.arn
+}
+
+output "invoke_arn" {
+  value = var.alias != null ? aws_lambda_alias.lambda_alias[0].invoke_arn : aws_lambda_function.lambda.invoke_arn
+}
